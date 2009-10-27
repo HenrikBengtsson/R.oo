8f251e33359357948ca899f00f055e7493165ee6
Henrik Bengtsson <hb@aroma-project.org>
2009-10-27 04:58:24 +0000

    Version: 1.6.3 [2009-10-26] o Added objectSize() for environments. o BUG FIX: Rdoc$compile() did not work with R v2.10.0 and newer.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.oo@293 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/objectSize.environment.R
@@ -0,0 +1,47 @@
+###########################################################################/**
+# @set "class=environment"
+# @RdocMethod objectSize
+#
+# @title "Gets the size of an environment in bytes"
+#
+# \description{
+#   @get "title".  
+# }
+#
+# @synopsis
+#
+# \arguments{
+#   \item{envir}{An @see "base::environment".}
+#   \item{...}{Arguments passed to @see "base::ls".}
+# }
+#
+# \value{
+#  Returns an @integer.
+# }
+#
+# @author
+#
+# \seealso{
+#   Internally @see "utils::object.size".
+# }
+#
+# \keyword{attribute}
+# \keyword{utilities}
+#*/###########################################################################
+setMethodS3("objectSize", "environment", function(envir, ...) {
+  names <- ls(envir=envir, all.names=TRUE, ...);
+  size <- 0;
+  for (name in names) {
+    size <- size + objectSize(envir[[name]]);
+  }
+  size;
+})
+
+
+
+
+############################################################################
+# HISTORY:
+# 2009-10-26
+# o Added objectSize() for environments.
+############################################################################
