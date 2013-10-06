02c00d5d1d9b4b05e18262030ef534be72db073a
Henrik Bengtsson <hb@aroma-project.org>
2013-10-06 00:01:35 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.oo@1751 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/000.R
+++ R/000.R
@@ -1,41 +1,4 @@
-##############################################################################
-# This code has to come first in a library. To do this make sure this file
-# is named "000.R" (zeros).
-##############################################################################
-
-
-############################################################################
-# HISTORY:
-# 2007-09-17
-# o Removed support for R v2.2.0 and before by removing patch for missing
-#   baseenv().
-# 2007-04-07
-# o Removed support for R v2.0.0 and before.
-# 2006-02-09
-# o Added baseenv() for R versions (< v2.2.0) where it does not exist.
-#   This is used in setGenericS3() and setMethodS3() from R v2.3.0.
-# 2005-02-15
-# o Now require() is only called for R v1.9.1 or eariler.
-# 2005-02-10
-# o Moved R.KEYWORDS into its own source file.
-# 2003-05-06
-# o Added require(methods) to make sure getMethods() etc works.
-# 2002-11-21
-# o Added "..." to R.KEYWORDS.
-# 2002-10-17
-# o Removed obsolete "modifiers<-"().
-# o Added also "Object" to the class attribute to make static methods to
-#   work.
-# 2002-10-16
-# o There are times when
-#     generic <- function(...) UseMethod() 
-#   is not working, for example
-#     fcn <- get("generic"); fcn(myObj, ...);
-#   For this reason, always do method dispatching using the name explicitly;
-#     generic <- function(...) UseMethod("generic") 
-#
-# 2002-10-15
-# o Created from R.oo Object.R and ideas as described on
-#    http://www.maths.lth.se/help/R/
-############################################################################
-
+## Look for existing generic functions also in imported namespaces.
+## This will affect whether setGenericS3() creates a generic function
+## or not.
+options("R.methodsS3:checkImports:setGenericS3"=TRUE)
