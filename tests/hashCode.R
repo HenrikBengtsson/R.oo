a1fc15470a370c46ec368f34f22f961c818ca944
hb <hb@aroma-project.org>
2016-01-30 12:45:37 -0800

    TESTS: Add tests for default hashCode()

---
+++ tests/hashCode.R
@@ -0,0 +1,27 @@
+library("R.oo")
+
+message("TESTING: hashCode() ...")
+
+y <- hashCode(character(0L))
+print(y)
+
+y <- hashCode(integer(0L))
+print(y)
+
+y <- hashCode(list(0L))
+print(y)
+
+y <- hashCode(as.list(1:10))
+print(y)
+
+y <- hashCode(base::letters)
+print(y)
+
+y <- hashCode(1:10)
+print(y)
+
+y <- hashCode(1:10+0.1)
+print(y)
+
+
+message("TESTING: hashCode() ... DONE")
