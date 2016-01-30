341211608a02a06e8f2e9063006cf43a5857e15d
hb <hb@aroma-project.org>
2016-01-30 12:11:09 -0800

    TESTS: Hmm... uses(<string>) doesn't play well with covr.

--- tests/Interface.R
+++ tests/Interface.R
@@ -12,9 +12,18 @@ int <- MyInterface()
 print(int)
 
 setConstructorS3("MyClass", function(...) {
-  extend(Object(), c("MyClass", uses("MyInterface")))
+  extend(Object(), c("MyClass", uses(MyInterface())))
 })
 obj <- MyClass()
 print(obj)
+
+
+if ("covr" %in% loadedNamespaces()) {
+  setConstructorS3("MyClass", function(...) {
+    extend(Object(), c("MyClass", uses("MyInterface")))
+  })
+  obj <- MyClass()
+  print(obj)
+}
 
 message("TESTING: Interface...DONE")
