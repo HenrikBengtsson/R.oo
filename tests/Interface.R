d5b79348d1a056141507d55e4b6072fe9c7ec1c1
hb <hb@aroma-project.org>
2016-01-30 11:00:34 -0800

    TESTS: Add test for Interface

---
+++ tests/Interface.R
@@ -0,0 +1,20 @@
+library("R.oo")
+
+message("TESTING: Interface...")
+
+int <- Interface()
+print(int)
+
+setConstructorS3("MyInterface", function(...) {
+  extend(Interface(), "MyInterface")
+})
+int <- MyInterface()
+print(int)
+
+setConstructorS3("MyClass", function(...) {
+  extend(Object(), c("MyClass", uses("MyInterface")))
+})
+obj <- MyClass()
+print(obj)
+
+message("TESTING: Interface...DONE")
