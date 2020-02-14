7b2f4b7926f9484bc3231bda14763338cfb3bbb3
Henrik Bengtsson <hb@aroma-project.org>
2020-02-13 20:38:32 -0800

    Protect more S3 methods for Object:s from being apply to non R.oo Object:s

--- R/zzz.avoid_Object_clashes.R
+++ R/zzz.avoid_Object_clashes.R
@@ -1,10 +1,16 @@
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-## WORKAROUND for namespace clashes between R.oo and rJava
-## where some R.oo S3 methods for Object and Exception
-## override the intended ones for rJava objects with
-## class attributes containing these classes as well.
+## WORKAROUND for clashes between R.oo Object:s and Object:s
+## unrelated to R.oo created by other packages.
 ##
+## Package 'rJava':
+## Some R.oo S3 methods for Object and Exception override
+## the intended ones for rJava objects with class attributes
+## containing these classes as well.
 ## See https://github.com/s-u/rJava/issues/60
+##
+## Package 'arrow':
+## `$<-.Object` causes problem for arrow:::as_arrow().
+## See https://github.com/HenrikBengtsson/R.oo/issues/21
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 .fixMethodS3 <- function(generic, class, expr=NULL, envir=parent.frame()) {
   method <- sprintf("%s.%s", generic, class)
@@ -31,10 +37,22 @@
 
 .isRoo <- function(x) is.environment(attr(x, ".env"))
 
+## rJava
 .fixMethodS3("names", "Object")
 .fixMethodS3("$", "Object")
-.fixMethodS3("$<-", "Object")
 .fixMethodS3("[[", "Object")
-.fixMethodS3("[[<-", "Object")
 .fixMethodS3("print", "Object")
 .fixMethodS3("print", "Exception")
+
+## arrow
+.fixMethodS3("$<-", "Object")
+
+## Some other, just in case
+.fixMethodS3("[[<-", "Object")
+.fixMethodS3("as.character", "Object")
+.fixMethodS3("clone", "Object")
+.fixMethodS3("extend", "Object")
+.fixMethodS3("ll", "Object")
+.fixMethodS3("names", "Object")
+.fixMethodS3("objectSize", "Object")
+.fixMethodS3("print", "Object")
